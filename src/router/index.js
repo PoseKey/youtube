231cf6ebf9a8227176a872ef45a9f74da2307acc
PoseKey <47172987+PoseKey@users.noreply.github.com>
2019-01-31 16:22:04 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Setting from '@/components/Setting'
+import Mirror from '@/components/Mirror'
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Setting',
+      component: Setting
+    },
+    {
+      path: '/Mirror',
+      name: "Mirror",
+      component: Mirror,
     }
   ]
 })
