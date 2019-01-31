4fa723cd3e9e7b5a07a2f34217b19ca4715958b3
PoseKey <47172987+PoseKey@users.noreply.github.com>
2019-01-31 16:21:16 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import { MdTabs, MdCard } from 'vue-material/dist/components'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import "vue-material/dist/theme/default.css";
+
+// Vue.use(MdTabs)
+// Vue.use(MdCard)
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
