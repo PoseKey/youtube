77c3a81726fc87c6b757a18248f2627dceb107f2
PoseKey <47172987+PoseKey@users.noreply.github.com>
2019-01-31 16:22:50 +0900

    Add files via upload

---
+++ src/components/Setting.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="Setting">
+    <!-- <md-card >
+          <md-avatar>
+          <img src="./assets/pose1.png" alt="Pose1">
+        </md-avatar>
+        <div class="md-title">Pose1 play/pause</div>
+          <md-card-header>
+            
+          </md-card-header>
+
+          <md-card-content>
+          </md-card-content>
+
+          <md-card-actions>
+          </md-card-actions>
+        </md-card>
+        <md-card >
+          <md-avatar>
+          <img src="./assets/pose2.png" alt="Pose2">
+        </md-avatar>
+        <div class="md-title">Pose2 volume up</div>
+          <md-card-header>
+            
+          </md-card-header>
+
+          <md-card-content>
+          </md-card-content>
+
+          <md-card-actions>
+            <md-button>Action</md-button> -->
+          <!-- </md-card-actions>
+        </md-card> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Setting',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
