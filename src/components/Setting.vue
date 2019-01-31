c11e1bb9d3f01d66d56c3fd9ff6e9f88bbb95d80
Ujoy7851 <joy7851@gmail.com>
2019-01-31 17:14:24 +0900

    update setting component

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,36 +1,72 @@
 <template>
   <div class="Setting">
     <!-- <md-card >
+      <md-avatar>
+      <img src="../assets/pose1.png" alt="Pose1">
+    </md-avatar>
+    <div class="md-title">Pose1 play/pause</div>
+      <md-card-header>
+        
+      </md-card-header>
+
+      <md-card-content>
+      </md-card-content>
+    </md-card>
+    <md-card >
+      <md-avatar>
+      <img src="../assets/pose2.png" alt="Pose2">
+    </md-avatar>
+    <div class="md-title">Pose2 volume up</div>
+      <md-card-header>
+        
+      </md-card-header>
+
+      <md-card-content>
+      </md-card-content>
+    </md-card> -->
+    
+    <md-table md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">Pose Description</h1>
+      </md-table-toolbar>
+
+      <md-table-row>
+        <md-table-head>Pose</md-table-head>
+        <md-table-head>Description</md-table-head>
+        <md-table-head>Skill</md-table-head>
+      </md-table-row>
+
+      <md-table-row>
+        <md-table-cell>
           <md-avatar>
-          <img src="./assets/pose1.png" alt="Pose1">
-        </md-avatar>
-        <div class="md-title">Pose1 play/pause</div>
-          <md-card-header>
-            
-          </md-card-header>
+            <img src="../assets/pose1.png" alt="Pose1">
+          </md-avatar>
+        </md-table-cell>
+        <md-table-cell>오른손을 든 자세</md-table-cell>
+        <md-table-cell>Play/Pause</md-table-cell>
+      </md-table-row>
 
-          <md-card-content>
-          </md-card-content>
-
-          <md-card-actions>
-          </md-card-actions>
-        </md-card>
-        <md-card >
+      <md-table-row>
+        <md-table-cell>
           <md-avatar>
-          <img src="./assets/pose2.png" alt="Pose2">
-        </md-avatar>
-        <div class="md-title">Pose2 volume up</div>
-          <md-card-header>
-            
-          </md-card-header>
+            <img src="../assets/pose2.png" alt="Pose1">
+          </md-avatar>
+        </md-table-cell>
+        <md-table-cell>왼손을 든 자세</md-table-cell>
+        <md-table-cell>Scroll Up</md-table-cell>
+      </md-table-row>
 
-          <md-card-content>
-          </md-card-content>
+      <md-table-row>
+        <md-table-cell>
+          <md-avatar>
+            <img src="../assets/pose3.png" alt="Pose1">
+          </md-avatar>
+        </md-table-cell>
+        <md-table-cell>양손을 든 자세</md-table-cell>
+        <md-table-cell>Scroll Down</md-table-cell>
+      </md-table-row>
+    </md-table>
 
-          <md-card-actions>
-            <md-button>Action</md-button> -->
-          <!-- </md-card-actions>
-        </md-card> -->
   </div>
 </template>
 
@@ -44,7 +80,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
