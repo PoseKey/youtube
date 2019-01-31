33085494602911aa986b2b75d05121b25a8806cf
PoseKey <47172987+PoseKey@users.noreply.github.com>
2019-01-31 16:21:39 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <md-toolbar class="md-primary">
+      <h1 class="md-title">PoseKey</h1>
+    </md-toolbar>
+    
+    <md-tabs class="md-primary" md-sync-route md-alignment="centered">
+      <md-tab id="tab-home" md-label="Settings" to="/">
+      </md-tab>
+      <md-tab id="tab-pages" md-label="Mirror" to="/Mirror"></md-tab>
+    </md-tabs>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -16,8 +24,14 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
+/* .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  } */
 </style>
