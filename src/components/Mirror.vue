d36a88a0994992ad46c12fa75546fc3cadd3fc93
Taeyeob <21300443@handong.edu>
2019-02-12 12:58:22 +0900

    dd

--- src/components/Mirror.vue
+++ src/components/Mirror.vue
@@ -8,6 +8,7 @@
             <a href="chrome-extension://pifojknhlbglpfoehbppiddjlgebooom/options.html" target="_blank">here</a>
             .</p>
         </div>
+        <div>All Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     </div>
 </template>
 
